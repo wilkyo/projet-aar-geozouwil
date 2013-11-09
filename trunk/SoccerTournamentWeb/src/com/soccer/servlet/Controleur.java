2eea0b0389d9d01e85522b97b973f31e6d6acb97
wfrancois45 <wfrancois45@63b79770-a631-c500-25bf-2faddcafcf7e>
2013-11-09 19:50:44 +0000

    Petites modifs de newteam (intégration des constantes); Récupération de l'EJB Facade Local avec annotation; L'appel à creerEquipe se fait bien :)

--- trunk/SoccerTournamentWeb/src/com/soccer/servlet/Controleur.java
+++ trunk/SoccerTournamentWeb/src/com/soccer/servlet/Controleur.java
@@ -1,23 +1,31 @@
 package com.soccer.servlet;
 
 import java.io.IOException;
+
+import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.soccer.ejb.facade.SoccerTournamentFacadeLocal;
+
 /**
  * Servlet implementation class Controleur
  */
 @WebServlet("/index")
 public class Controleur extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-	private static final String PATH = "/pages/";
-	private static final String ACTION_NEW_TEAM = "newteam";
-	private static final String ACTION_LOGIN = "login";
-	private static final String ACTION_ADD = "ajout";
+	public static final long serialVersionUID = 1L;
+	public static final String WEB_PATH = "/SoccerTournamentWeb/";
+	public static final String PAGES_PATH = "pages/";
+	public static final String CSS_PATH = "styles/";
+	public static final String ACTION_NEW_TEAM = "newteam";
+	public static final String ACTION_LOGIN = "login";
+	public static final String ACTION_ADD = "ajout";
 
+	@EJB
+	private SoccerTournamentFacadeLocal facade;
 
 	/**
 	 * Default constructor.
@@ -47,20 +55,27 @@ public class Controleur extends HttpServlet {
 		// si l'action est nulle ou l'action égale à home
 		if ((action == null) || (action.equals("home"))) {
 			// direction la page d'accueil
-			request.getRequestDispatcher(PATH + "index.jsp").forward(request,
-					response);
+			request.getRequestDispatcher(PAGES_PATH + "index.jsp").forward(
+					request, response);
 		}
 		// si l'action est l'inscription d'une équipe
 		else if (action.equals(ACTION_NEW_TEAM)) {
+			String nomEquipe = request.getParameter("nomEquipe");
+			String nomRepresentant = request.getParameter("nomRepresentant");
+			String prenomRepresentant = request
+					.getParameter("prenomRepresentant");
+			String[] nomsJoueurs = request.getParameterValues("nom[]");
+			String[] prenomsJoueurs = request.getParameterValues("prenom[]");
+			// Nouvelle equipe
+			if (nomEquipe != null && nomRepresentant != null
+					&& prenomRepresentant != null && nomsJoueurs != null
+					&& prenomsJoueurs != null) {
+				facade.creerEquipe(nomEquipe, nomRepresentant,
+						prenomRepresentant, nomsJoueurs, prenomsJoueurs);
+			}
 			// direction la page d'inscription d'une équipe
-			request.getRequestDispatcher(PATH + "newteam.jsp").forward(request,
-					response);
-		}
-		//Nouvelle equipe
-		else if(action.equals(ACTION_ADD)){
-			String []nom=request.getParameterValues("nom[]");
-			String []prenom=request.getParameterValues("prenom[]");
-			System.out.println(nom.length);
+			request.getRequestDispatcher(PAGES_PATH + "newteam.jsp").forward(
+					request, response);
 		}
 		// si l'action est de se connecter ( pour l'admin)
 		else if (action.equals(ACTION_LOGIN)) {
@@ -70,8 +85,8 @@ public class Controleur extends HttpServlet {
 				System.err.println("Logging... " + login + " -> " + passwd);
 			}
 			// direction la page d'inscription
-			request.getRequestDispatcher(PATH + "login.jsp").forward(request,
-					response);
+			request.getRequestDispatcher(PAGES_PATH + "login.jsp").forward(
+					request, response);
 		} else if (action.equals("404")) {
 			System.err.println("404");
 		}
