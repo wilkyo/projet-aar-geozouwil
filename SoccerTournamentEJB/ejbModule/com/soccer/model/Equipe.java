e64a592fe7395ba1fec94f3074999ffac113c973
zo.fabrice <zo.fabrice@63b79770-a631-c500-25bf-2faddcafcf7e>
2013-11-09 00:05:22 +0000

    Add Annotations to model

--- SoccerTournamentEJB/ejbModule/com/soccer/model/Equipe.java
+++ SoccerTournamentEJB/ejbModule/com/soccer/model/Equipe.java
@@ -1,44 +1,78 @@
 package com.soccer.model;
 
+import java.io.Serializable;
 import java.util.List;
 
-public class Equipe {
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
+@Entity
+public class Equipe  implements Serializable  {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
 	private String nom;
 	private List<Joueur> joueurs;
 	private String nomRepresentant;
 	private String prenomRepresentant;
-
+	
+	/**
+	 * @return the nom
+	 */
 	public String getNom() {
 		return nom;
 	}
-
+	/**
+	 * @param nom the nom to set
+	 */
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
-
-	public List<Joueur> getJoueurs() {
-		return joueurs;
-	}
-
-	public void setJoueurs(List<Joueur> joueurs) {
-		this.joueurs = joueurs;
-	}
-
+	
+	/**
+	 * @return the nomRepresentant
+	 */
 	public String getNomRepresentant() {
 		return nomRepresentant;
 	}
-
+	/**
+	 * @param nomRepresentant the nomRepresentant to set
+	 */
 	public void setNomRepresentant(String nomRepresentant) {
 		this.nomRepresentant = nomRepresentant;
 	}
-
+	/**
+	 * @return the prenomRepresentant
+	 */
 	public String getPrenomRepresentant() {
 		return prenomRepresentant;
 	}
-
+	/**
+	 * @param prenomRepresentant the prenomRepresentant to set
+	 */
 	public void setPrenomRepresentant(String prenomRepresentant) {
 		this.prenomRepresentant = prenomRepresentant;
+	}
+
+	/**
+	 * Get Players.
+	 * 
+	 * @return the joueurs
+	 */
+	@OneToMany(mappedBy="equipe",cascade={CascadeType.ALL})
+	public List<Joueur> getJoueurs() {
+		return joueurs;
+	}
+	/**
+	 * @param joueurs the joueurs to set
+	 */
+	public void setJoueurs(List<Joueur> joueurs) {
+		this.joueurs = joueurs;
 	}
 
 }
--- SoccerTournamentEJB/ejbModule/com/soccer/model/Tournoi.java
+++ SoccerTournamentEJB/ejbModule/com/soccer/model/Tournoi.java
@@ -1,35 +1,65 @@
 package com.soccer.model;
 
+import java.io.Serializable;
 import java.util.List;
 
-public class Tournoi {
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
+@Entity
+public class Tournoi  implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
 	private String nom;
-	private List<Rencontre> rencontres;
 	private int tourActuel;
-
+	private List<Rencontre> rencontres;
+	
+	/**
+	 * @return the nom
+	 */
 	public String getNom() {
 		return nom;
 	}
-
+	/**
+	 * @param nom the nom to set
+	 */
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
-
-	public List<Rencontre> getRencontres() {
-		return rencontres;
-	}
-
-	public void setRencontres(List<Rencontre> rencontres) {
-		this.rencontres = rencontres;
-	}
-
+	/**
+	 * @return the tourActuel
+	 */
 	public int getTourActuel() {
 		return tourActuel;
 	}
-
+	/**
+	 * @param tourActuel the tourActuel to set
+	 */
 	public void setTourActuel(int tourActuel) {
 		this.tourActuel = tourActuel;
 	}
+	/**
+	 * Got all Match
+	 * 
+	 * @return the rencontres
+	 */
+	@OneToMany(mappedBy="tournoi",cascade={CascadeType.ALL})
+	public List<Rencontre> getRencontres() {
+		return rencontres;
+	}
+	/**
+	 * @param rencontres the rencontres to set
+	 */
+	public void setRencontres(List<Rencontre> rencontres) {
+		this.rencontres = rencontres;
+	}
+	
+	
 
 }
