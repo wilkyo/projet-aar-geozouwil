bf9c613d1c58a99e22f4d93a022dd404e0a89923
geocrochet <geocrochet@gmail.com>
2013-11-30 11:33:12 +0000

    javadoc EJB facade

--- SoccerTournamentEJB/ejbModule/com/soccer/ejb/facade/SoccerTournamentFacadeLocal.java
+++ SoccerTournamentEJB/ejbModule/com/soccer/ejb/facade/SoccerTournamentFacadeLocal.java
@@ -2,6 +2,9 @@ package com.soccer.ejb.facade;
 
 import javax.ejb.Local;
 
+/**
+ * The local interface of SoccerTournamentFacade.
+ */
 @Local
 public interface SoccerTournamentFacadeLocal extends SoccerTournamentFacade {
 }
